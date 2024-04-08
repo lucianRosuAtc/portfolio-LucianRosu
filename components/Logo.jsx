48fbd67a392bfca378ad11c56d2b9a1882579004
Lucian Rosu <lucian.rosu.atc@gmail.com>
2024-04-08 16:47:43 +0100

    Update Logo component to include Link and Image in Logo.jsx

--- components/Logo.jsx
+++ components/Logo.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import Link from "next/link";
+import Image from "next/image";
 
 export default function Logo() {
   return (
-    <div>
-      Logo
-    </div>
-  )
+    <Link href="/">
+      <Image
+        src="/img/DevLuc.png"
+        width={240}
+        height={60}
+        alt="Logo"
+        className="object-cover w-36 md:w-60 md:h-16 rounded-xl border-2 border-[#DBC078] cursor-pointer"
+      />
+    </Link>
+  );
 }
