2b44854464ae56a613b819fd536cac4f3629d111
Lucian Rosu <lucian.rosu.atc@gmail.com>
2024-04-09 17:00:27 +0100

    Refactor MobileNav component to use Sheet, SheetContent, and SheetTrigger in MobileNav.jsx

--- components/MobileNav.jsx
+++ components/MobileNav.jsx
@@ -1,9 +1,23 @@
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/Sheet";
+import { AlignJustify } from "lucide-react";
 
+import NavBar from "./NavBar";
+import Logo from "./Logo";
+import Socials from "./Socials";
 
 export default function MobileNav() {
   return (
-    <div>
-      Mobile Nav
-    </div>
-  )
+    <Sheet>
+      <SheetTrigger asChild>
+        <AlignJustify className="cursor-pointer" />
+      </SheetTrigger>
+      <SheetContent>
+        <div className="flex flex-col items-center gap-8">
+          <Logo />
+          <NavBar />
+          <Socials />
+        </div>
+      </SheetContent>
+    </Sheet>
+  );
 }
