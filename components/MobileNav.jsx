c89721db28996086ac5f3956cd07ba1a597c068e
Lucian Rosu <lucian.rosu.atc@gmail.com>
2024-04-10 13:20:18 +0100

    Update MobileNav component styles in MobileNav.jsx

--- components/MobileNav.jsx
+++ components/MobileNav.jsx
@@ -20,7 +20,7 @@ export default function MobileNav() {
   return (
     <Sheet>
       <SheetTrigger>
-        <AlignJustify className="cursor-pointer" />
+        <AlignJustify className="cursor-pointer"/>
       </SheetTrigger>
       <SheetContent className="dark:bg-lightDark w-full">
         <div className="flex flex-col items-center justify-between h-full py-8">
