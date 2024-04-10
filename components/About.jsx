ae3eea8c9b77a597e7b75a7f5c6841bd81293873
Lucian Rosu <lucian.rosu.atc@gmail.com>
2024-04-10 17:56:43 +0100

    Refactor About component to update role names and add flex layout in About.jsx

--- components/About.jsx
+++ components/About.jsx
@@ -65,17 +65,17 @@ const qualificationData = [
     data: [
       {
         company: "Adopstar",
-        qualification: "Web Developer",
+        role: "Web Developer",
         years: "2023-2024",
       },
       {
-        university: "University of Scrimba",
-        qualification: "Diploma in Web Development",
+        company: "Scrimba",
+        role: "Diploma in Web Development",
         years: "2018-2019",
       },
       {
-        university: "University of Intequal",
-        qualification: "Diploma in Software Development",
+        company: "Frelancer",
+        role: "Diploma in Software Development",
         years: "2023-2024",
       },
     ],
@@ -130,7 +130,7 @@ export default function About() {
     return arr.find((item) => item.title === title);
   };
 
-  console.log(qualificationData);
+  // console.log(qualificationData);
   return (
     <section className="xl:h-[860px] pb-12 my-3 md:my-7 xl:my-12">
       <div className="flex flex-col px-4 md:px-12 mx-auto">
@@ -194,14 +194,18 @@ export default function About() {
                         </h4>
                       </div>
 
-                      <div className="">
+                      <div className="flex flex-col gap-y-8">
                         {getData(qualificationData, "experience").data.map(
                           (item, index) => {
-                            const { company, qualification, years } = item;
+                            const { company, role, years } = item;
                             return (
-                              <div className="" key={index}>
+                              <div className="flex gap-x-8 group bg-orange-100" key={index}>
+                                <div className="h-[84px] w-[1px] bg-border relative ml-2"></div>  
+                                {/* /// //// */}
                                 <div className="">
-                                  <div className="">{company}</div>
+                                  <div className="font-semibold text-xl leading-none mb-2">{company}</div>
+                                  <div className="text-lg leading-none text-muted-foreground mb-4">{role}</div>
+                                  <div className="text-base font-medium">{years}</div>
                                 </div>
                               </div>
                             );
