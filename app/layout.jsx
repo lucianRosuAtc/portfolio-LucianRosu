107269ca589da25f25057e6c99dcfcb0d7cd6b86
Lucian Rosu <lucian.rosu.atc@gmail.com>
2024-04-10 13:22:35 +0100

    Update ThemeProvider attribute in layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -15,7 +15,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <ThemeProvider>
+        <ThemeProvider attribute='class'>
           <Header />
           {children}
           <Footer />
