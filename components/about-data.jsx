42d85966864f161fabdb3d2ee7ba3b6760329a99
Lucian Rosu <lucian.rosu.atc@gmail.com>
2024-04-11 20:00:22 +0100

    Refactor About component to use separate data files and update Lucide icons import

---
+++ components/about-data.jsx
@@ -0,0 +1,133 @@
+import {
+    User2,
+    MailIcon,
+    HomeIcon,
+    PhoneCall,
+    GraduationCap,
+    Calendar,
+  } from "lucide-react";
+  
+  export const infoData = [
+    {
+      icon: <User2 size={20} />,
+      text: "Lucian Rosu",
+    },
+    {
+      icon: <PhoneCall size={20} />,
+      text: "07505709297",
+    },
+    {
+      icon: <MailIcon size={20} />,
+      text: "lucian.rosu.atc@gmail.com",
+    },
+    {
+      icon: <Calendar size={20} />,
+      text: "Born on 17/06/1983",
+    },
+    {
+      icon: <GraduationCap size={20} />,
+      text: "University of Leads",
+    },
+    {
+      icon: <HomeIcon size={20} />,
+      text: "Exeter, UK",
+    },
+  ];
+  
+  export const qualificationData = [
+    {
+      title: "education",
+      data: [
+        {
+          university: "University of Leads",
+          qualification: "BSc in Computer Science",
+          years: "2010-2014",
+        },
+        {
+          university: "University of Scrimba ",
+          qualification: "Diploma in Web Development",
+          years: "2018-2019",
+        },
+        {
+          university: "University of Intequal",
+          qualification: "Diploma in Software Development",
+          years: "2023-2024",
+        },
+      ],
+    },
+    {
+      title: "experience",
+      data: [
+        {
+          company: "Adopstar",
+          role: "Web Developer",
+          years: "2023-2024",
+        },
+        {
+          company: "Scrimba",
+          role: "Diploma in Web Development",
+          years: "2018-2019",
+        },
+        {
+          company: "Frelancer",
+          role: "Diploma in Software Development",
+          years: "2023-2024",
+        },
+      ],
+    },
+  ];
+  
+  export const skillData = [
+    {
+      title: "skills",
+      data: [
+        {
+          name: "HTML",
+        },
+        {
+          name: "CSS",
+        },
+        {
+          name: "Tailwindcss",
+        },
+        {
+          name: "JavaScript",
+        },
+        {
+          name: "React",
+        },
+        {
+          name: "Nextjs",
+        },
+        {
+          name: "TipeScript",
+        },
+        {
+          name: "Vercel",
+        },
+        {
+          name: "Zepelin",
+        },
+        {
+          name: "Figma",
+        },
+      ],
+    },
+    {
+      title: "tools",
+      data: [
+        {
+          imgPath: "/img/about/VsCode.webp",
+        },
+        {
+          imgPath: "/img/about/VsCode.webp",
+        },
+        {
+          imgPath: "/img/about/VsCode.webp",
+        },
+        {
+          imgPath: "/img/about/VsCode.webp",
+        },
+      ],
+    },
+  ];
