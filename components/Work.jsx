96a10d4099616975bc96f84d2d301397e5a37b51
Lucian Rosu <lucian.rosu.atc@gmail.com>
2024-04-12 15:56:16 +0100

    Update GitHub and Link icons in Work.jsx

--- components/Work.jsx
+++ components/Work.jsx
@@ -2,8 +2,9 @@
 
 import Link from "next/link";
 import { Button } from "./ui/button";
-import { Card, CardHeader, CardDescription } from "./ui/card";
-import { Github, Link2Icon } from "lucide-react";
+import { Card, CardDescription } from "./ui/card";
+import { RiGithubFill } from "react-icons/ri";
+import { Link2Icon } from "lucide-react";
 import { Badge } from "./ui/badge";
 import Image from "next/image";
 
@@ -107,7 +108,7 @@ export default function Work() {
           <Slider {...settings} className="mx-auto max-w-7xl px-4 md:px-0">
             {projectData.map((project, index) => (
               <div key={index} className="mx-auto">
-                <Card className="mx-2 md:mx-4 shadow-xl dark:bg-[#0C0A09] dark:shadow-secondary dark:shadow-lg max-w-[400px] mb-12">
+                <Card className="group relative mx-2 md:mx-4 shadow-xl dark:bg-[#0C0A09] dark:shadow-secondary dark:shadow-lg max-w-[400px] mb-12">
                   <Badge className="absolute top-6 ml-4 md:ml-8 mb-2 uppercase text-sm font-medium shadow-lg dark:shadow-lg dark:shadow-secondary">
                     {project.category}
                   </Badge>
@@ -119,6 +120,21 @@ export default function Work() {
                     className="shadow-lg dark:shadow-lg dark:shadow-secondary w-[290px] h-80 mx-auto object-cover object-center rounded-lg px-1 mt-4"
                     priority="true"
                   />
+
+                  <div className="flex justify-center">
+                    <Link
+                      href={project.gitHub}
+                      className="absolute top-1/3 transform -translate-y-1/2 flex justify-center items-center bg-secondary w-[54px] h-[54px] rounded-full scale-0 opacity-0 group-hover:scale-100 group-hover:opacity-100 transition-all duration-300 text-white hover:text-primary hover:border-2 hover:border-primary"
+                    >
+                      <RiGithubFill size={34}/>
+                    </Link>
+                    <Link
+                      href={project.link}
+                      className="absolute top-1/2 transform -translate-y-1/2 flex justify-center items-center bg-secondary w-[54px] h-[54px] rounded-full scale-0 opacity-0 group-hover:scale-100 group-hover:opacity-100 transition-all duration-300 text-white hover:text-primary hover:border-2 hover:border-primary"
+                    >
+                      <Link2Icon />
+                    </Link>
+                  </div>
                   <h4 className="px-4 py-5">{project.name}</h4>
                   <CardDescription className="text-muted-foreground text-lg mb-2 font-light line-clamp-2 px-5 pb-16">
                     {project.description}
