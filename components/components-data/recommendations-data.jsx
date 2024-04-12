12f7cc1bc2aefc75530feb230b9eb4cd784e2690
Lucian Rosu <lucian.rosu.atc@gmail.com>
2024-04-12 18:36:52 +0100

    Update carousel settings and remove unnecessary code in Recommendations.jsx and WorkCarousel.jsx

--- components/components-data/recommendations-data.jsx
+++ components/components-data/recommendations-data.jsx
@@ -5,7 +5,6 @@ export const recommendationsData = [
     job:"Software Developer",
     description:
       "Lucian Rosu is one of the best among all the people I have ever worked with. He is a very productive person, hardworking, broad-minded and forward-thinking individual. Intelligent, ambitious, energetic and proactive perfectionist. The desire for proficiency make Lucian a valuable asset to the team.",
-    link: "https://www.linkedin.com/in/lucian-rosu-atc/details/recommendations/?detailScreenTabIndex=0",
   },
   {
     img: "/img/recommendations/Gilson.jpg",
@@ -13,7 +12,6 @@ export const recommendationsData = [
     job:"Full Stack Developer",
     description:
       "Lucian collaborated with ona project. During this period, he proved that he is an ambitious, hardworking person with good technical skills who has a strong desire to learn even more and improve himself. He works very well in the team and is always willing to help. Lucian is a person I highly recommend.",
-    link: "https://www.linkedin.com/in/lucian-rosu-atc/details/recommendations/?detailScreenTabIndex=0",
   },
   {
     img: "/img/recommendations/Amber.jpg",
@@ -21,7 +19,6 @@ export const recommendationsData = [
     job:"Full Stack Web Developer",
     description:
       "Lucian is a fast learner with a unique approach to solving problems. Lucian is not afraid to dive into challenging tasks with a positive attitude. He is a great contributor to any team. I look forward to working with Lucian in the future and highly recommend him for any projects.",
-    link: "https://www.linkedin.com/in/lucian-rosu-atc/details/recommendations/?detailScreenTabIndex=0",
   },
   {
     img: "/img/recommendations/Miguel.jpg",
@@ -29,6 +26,5 @@ export const recommendationsData = [
     job:"Frontend Developer",
     description:
       "I met Lucian Rosu while we were studying at Scrimba. Lucian is smart, professional and deadline oriented. Not only did he become a great developer, but he was encouraging and helping the rest of us to achieve it. Highly recommended.",
-    link: "https://www.linkedin.com/in/lucian-rosu-atc/details/recommendations/?detailScreenTabIndex=0",
   },
 ];
