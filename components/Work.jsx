a4aecfee761110860c0dd72fd71506e9f8fb07fc
Lucian Rosu <lucian.rosu.atc@gmail.com>
2024-04-12 14:32:39 +0100

    Refactor layout, page, About, and Services components

--- components/Work.jsx
+++ components/Work.jsx
@@ -1,142 +1,137 @@
-'use client'
+"use client";
 
-import Link from "next/link"
-import { Button } from "./ui/button" 
-
+import Link from "next/link";
+import { Button } from "./ui/button";
+import { Card, CardHeader, CardDescription } from "./ui/card";
+import { Github, Link2Icon } from "lucide-react";
+import { Badge } from "./ui/badge";
+import Image from "next/image";
 
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
 
-const projectData =[
+const projectData = [
   {
-    img:'/img/work/Fun with Sanity.webp',
-    category:'Nextjs & Sanity',
-    name: 'Fun with Sanity',
-    description: 'A simple blog website built with Nextjs and Sanity.io',
-    link:'https://sanity-nextjs-lucian.vercel.app/',
-    gitHub:'/',
+    img: "/img/work/Fun with Sanity.webp",
+    category: "full-stack",
+    name: "Fun with Sanity",
+    description:
+      "A straightforward blog website constructed with Nextjs and Sanity.io.",
+    link: "https://sanity-nextjs-lucian.vercel.app/",
+    gitHub: "/",
   },
   {
-    img:'/img/work/TMDB App.webp',
-    category:'Nextjs',
-    name: 'The movie Data Base',
-    description: 'Calling the movie Database api to have an update with the latest movies',
-    link:'https://tmdb-api-eta.vercel.app/',
-    gitHub:'/',
+    img: "/img/work/TMDB App.webp",
+    category: "Nextjs",
+    name: "The Movie Data Base",
+    description:
+      "Invoking The Movie Database API to maintain an application that stays updated with the latest films.",
+    link: "https://tmdb-api-eta.vercel.app/",
+    gitHub: "/",
   },
   {
-    img:'/img/work/Media Player.webp',
-    category:'Javascript',
-    name: 'Music Player',
-    description: 'A simple music player built with HTML, CSS and Javascript',
-    link:'https://music-player-13.netlify.app/',
-    gitHub:'/',
+    img: "/img/work/Media Player.webp",
+    category: "JavaScript",
+    name: "Music Player",
+    description:
+      "A simple music player constructed with HTML, CSS and Javascript",
+    link: "https://music-player-13.netlify.app/",
+    gitHub: "/",
   },
-]
+  {
+    img: "/img/work/Password Generator.png",
+    category: "JavaScript",
+    name: "Password Generator",
+    description:
+      "A Password Generator able to generate paswords with a length between 10 and 16 caracteres constructed with HTML, CSS and Javascript",
+    link: "https://password-generator13.netlify.app/",
+    gitHub: "/",
+  },
+];
 
 export default function Work() {
-    const settings = {
-        dots: false,
-        infinite: true,
-        speed: 2500,
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        arrows:true,
-        centerMode: true,
-        centerPadding: "0px",
-        autoplay: true,
-        autoplaySpeed: 3000,
-        prevArrow: <samplePrevArrow currentSlide={0} slideCount={0} />,
-        nextArrow: <sampleNextArrow currentSlide={0} slideCount={0} />,
-        responsive: [
-          {
-            breakpoint: 1024,
-            settings: {
-              slidesToShow: 3,
-              slidesToScroll: 3,
-              infinite: true,
-              dots: false,
-            },
-          },
-          {
-            breakpoint: 768,
-            settings: {
-              slidesToShow: 2,
-              slidesToScroll: 2,
-            },
-          },
-            {
-                breakpoint: 520,
-                settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1,
-                },
-            },
-        ],
-    }
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 2500,
+    autoplaySpeed: 3000,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: false,
+    centerMode: true,
+    centerPadding: "0",
+    autoplay: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: false,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 520,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+
   return (
-    <div className="relative bg-orange-100">
-        <Slider {...settings} className="z-10 m-auto flex items-center justify-center text-center max-h-[580px] max-w-5xl">
-                        {projectData.map((project, index) => (
-                        <div key={index} className="flex flex-col items-center justify-center">
-                                <img src={project.img} alt="project" className="w-64 h-64 object-cover rounded-lg" />
-                        </div>
-                        ))}
-                 </Slider>
-                </div>
-            )
-        }
 
 
-    const samplePrevArrow = ({
-        curentSlide,
-        slideCount,
-        ...props
-    }) => (
-        <button
-        {...props}
-        className="absolute top-1/2 left-0 z-10"
-        >
-            <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-6 w-6"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            >
-                <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M15 19l-7-7 7-7"
-                />
-            </svg>
-        </button>
-     )
+    <div className="relative mb-12 xl:mb-48">
+      <div className="container mx-auto">
+        <div className="flex flex-col justify-center items-center xl:items-start mb-12 max-w-sm text-center mx-auto xl:mx-0 xl:text-left xl:h-80">
+          <h2 className="mb-4">Latest Projects</h2>
+          <p className="subtitle mb-8">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Consequatur, saepe?
+          </p>
+          <Link href="/projects">
+            <Button>All Projects</Button>
+          </Link>
+        </div>
 
-     const sampleNextArrow = ({
-        curentSlide,
-        slideCount,
-        ...props    
-    }) => ( 
-        <button
-        {...props}
-        className="absolute top-1/2 right-0 z-10"
-        >
-            <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-6 w-6"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            >
-                <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M9 5l7 7-7 7"
-                />
-            </svg>
-        </button>
-    )
+
+        <div className="">
+          <Slider {...settings} className="">
+
+            {projectData.map((project, index) => (
+              <div key={index} >
+
+                <Card className="mx-1 md:mx-4">
+                <Badge className="absolute top-6 ml-4 md:ml-14 mb-2 uppercase text-sm font-medium">
+  {project.category}
+</Badge>
+                <Image
+                  src={project.img}
+                  alt="project"
+                  width={300}
+                  height={300}
+                  className=" shadow-2xl w-[300px] md:w-80 h-80 mx-auto object-cover object-center rounded-lg mt-4"
+                  priority="true"
+                  />
+                <CardHeader className="">{project.name}</CardHeader>
+                    <CardDescription className="subtitle px-5">{project.description}</CardDescription>
+                  </Card>
+              </div>
+            ))}
+          </Slider>
+        </div>
+      </div>
+    </div>
+  );
+}
