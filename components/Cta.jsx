e6aa48cac129a00762a8908b4cec40d93efde03e
Lucian Rosu <lucian.rosu.atc@gmail.com>
2024-04-13 12:42:48 +0100

    Refactor layout, page, and components in app directory

---
+++ components/Cta.jsx
@@ -0,0 +1,20 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
+export default function Cta() {
+  return (
+    <div className="py-24 bg-orange-100 dark:bg-secondary/40">
+      <div className="container mx-auto">
+        <div className="flex flex-col items-center">
+          <h2 className="max-w-xl text-center mb-8">
+            Ready to bring your project to life? Let's turn your ideas into
+            reality. Get in touch today!
+          </h2>
+          <Link href="/contact">
+            <Button className="gap-2 px-9">Contact Me</Button>
+            </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
