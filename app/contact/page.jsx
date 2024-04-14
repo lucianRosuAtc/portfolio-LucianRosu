857073c7b04c837cc593793d9891c2fc7138fa5c
Lucian Rosu <lucian.rosu.atc@gmail.com>
2024-04-14 17:57:58 +0100

    Refactor Contact page to update layout and add contact information

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import { HomeIcon, PhoneIcon, MailIcon} from 'lucide-react'
 
 export default function Contact() {
   return (
-    <section className='min-h-screen'>
-      Contact
+    <section className=''>
+     <div className="container mx-auto">
+      <div className="flex flex-col justify-center">
+        <div className="flex items-center gap-x-4 text-primary text-lg mb-4">
+           Say Hello
+        </div>
+        <h1 className="">Let's work together</h1>
+        <p className="max-w-[600px]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque corrupti vero expedita dignissimos natus reprehenderit. Rerum corporis perferendis consequatur ipsam.</p>
+      </div>
+      <div className="">
+
+      </div>
+      <div className="flex items-center gap-x-8 ">
+        <MailIcon size={18} className='text-primary'/>
+        <div className="">lucian.rosu.atc@gmail.com</div>
+        <form>Form</form>
+         </div>
+     </div>
     </section>
   )
 }
