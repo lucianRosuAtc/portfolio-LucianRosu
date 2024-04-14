7f3111ee455a733510742711ce1aa45a49529244
Lucian Rosu <lucian.rosu.atc@gmail.com>
2024-04-14 17:24:09 +0100

    Refactor Projects page to use Tabs component, update code formatting, and improve layout

--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -29,7 +29,7 @@ export default function Projects() {
     <section className="min-h-screen pt-12">
       <div className="container mx-auto">
         <h2 className="mb-8 xl:mb-16 text-center mx-auto">My Projects</h2>
-        <Tabs defaultValue={category}>
+        <Tabs defaultValue={category} className="mb-12 xl:mb-24">
           <TabsList className=" grid md:grid-cols-4 w-full max-w-[640px] h-full mb-12 mx-auto md:border dark:border-none">
             {categories.map((category, index) => {
               return (
@@ -44,7 +44,7 @@ export default function Projects() {
               );
             })}
           </TabsList>
-          <div className="container grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 mx-auto gap-4">
+          <div className="container grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 mx-auto md:gap-4">
             {filteredProjects.map((project, index) => (
               <div key={index}>
 
@@ -58,7 +58,7 @@ export default function Projects() {
                     alt="project"
                     width={300}
                     height={300}
-                    className="shadow-lg dark:shadow-lg dark:shadow-secondary w-[290px] h-80 mx-auto object-cover object-center rounded-lg px-1 mt-4"
+                    className="shadow-lg dark:shadow-lg dark:shadow-secondary w-auto h-80 mx-auto object-cover object-center rounded-lg px-1 mt-4"
                     priority="true"
                   />
 
