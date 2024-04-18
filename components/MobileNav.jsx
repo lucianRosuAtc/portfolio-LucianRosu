215c90ee8363a8813a0366e4b2ef43cce839c50b
Lucian Rosu <lucian.rosu.atc@gmail.com>
2024-04-18 16:53:16 +0100

    Update styling and add new classes in globals.css, Logo.jsx, NavBar.jsx, and Footer.jsx components

--- components/MobileNav.jsx
+++ components/MobileNav.jsx
@@ -1,3 +1,85 @@
+// import {
+//   Sheet,
+//   SheetContent,
+//   SheetTrigger,
+//   SheetClose,
+// } from "@/components/ui/sheet";
+// import { AlignJustify } from "lucide-react";
+
+// import { usePathname } from "next/navigation";
+// import { motion } from "framer-motion";
+
+// import Link from "next/link";
+// import Image from "next/image";
+
+// import { navigationlinks } from "./components-data/myLinks-data";
+// import { social } from "./components-data/myLinks-data";
+
+// export default function MobileNav() {
+//   const pathname = usePathname();
+//   return (
+//     <Sheet>
+//       <SheetTrigger>
+//         <AlignJustify className="cursor-pointer"/>
+//       </SheetTrigger>
+//       <SheetContent className="dark:bg-lightDark w-full">
+//         <div className="flex flex-col items-center justify-between h-full py-8">
+//           <div className="flex flex-col items-center justify-between gap-y-24">
+//             <SheetClose asChild>
+//               <Link href="/">
+//                 <Image
+//                   src="/img/logo&header/DevLuc.png"
+//                   width={240}
+//                   height={60}
+//                   alt="Logo"
+//                   className="object-cover w-60 h-16 rounded-md border-2 border-goldText cursor-pointer mt-12"
+//                   priority
+//                 />
+//               </Link>
+//             </SheetClose>
+//             <div className="flex flex-col items-center text-2xl gap-y-8">
+//               {navigationlinks.map((navlink) => (
+//                 <SheetClose asChild key={navlink.url}>
+//                   <Link
+//                     href={navlink.url}
+//                     className="relative hover:text-primary transition-all"
+//                   >
+//                     {navlink.url === pathname && (
+//                       <motion.span
+//                         initial={{ y: "-100%" }}
+//                         animate={{ y: 0 }}
+//                         transition={{ type: "tween" }}
+//                         layoutId="underline"
+//                         className="absolute left-0 top-full h-[2px] bg-primary w-full"
+//                       />
+//                     )}
+//                     {navlink.name}
+//                   </Link>
+//                 </SheetClose>
+//               ))}
+//             </div>
+//           </div>
+//           <SheetClose asChild>
+//             <div className="flex gap-x-8">
+//               {social.map((item, index) => (
+//                 <a
+//                   href={item.url}
+//                   key={index}
+//                   target="_blank"
+//                   className="text-4xl hover:text-primary"
+//                 >
+//                   {item.name}
+//                 </a>
+//               ))}
+//             </div>
+//           </SheetClose>
+//         </div>
+//       </SheetContent>
+//     </Sheet>
+//   );
+// }
+
+"use client";
 import {
   Sheet,
   SheetContent,
@@ -5,54 +87,50 @@ import {
   SheetClose,
 } from "@/components/ui/sheet";
 import { AlignJustify } from "lucide-react";
-
 import { usePathname } from "next/navigation";
-import { motion } from "framer-motion";
 
 import Link from "next/link";
 import Image from "next/image";
 
 import { navigationlinks } from "./components-data/myLinks-data";
 import { social } from "./components-data/myLinks-data";
+import Logo from "./Logo";
 
 export default function MobileNav() {
   const pathname = usePathname();
   return (
     <Sheet>
       <SheetTrigger>
-        <AlignJustify className="cursor-pointer"/>
+        <AlignJustify className="cursor-pointer" />
       </SheetTrigger>
       <SheetContent className="dark:bg-lightDark w-full">
         <div className="flex flex-col items-center justify-between h-full py-8">
-          <div className="flex flex-col items-center justify-between gap-y-24">
+          <div className="flex flex-col items-center justify-between gap-y-12">
+            {/* MARK: if I call Logo as a component the sheet won't close
+             */}
             <SheetClose asChild>
               <Link href="/">
                 <Image
                   src="/img/logo&header/DevLuc.png"
                   width={240}
-                  height={60}
+                  height={64}
                   alt="Logo"
-                  className="object-cover w-60 h-16 rounded-md border-2 border-goldText cursor-pointer mt-12"
+                  className="object-cover w-36 md:w-60 h-10 md:h-16 rounded-md border-2 border-goldText cursor-pointer mt-12"
                   priority
                 />
               </Link>
             </SheetClose>
+
             <div className="flex flex-col items-center text-2xl gap-y-8">
               {navigationlinks.map((navlink) => (
                 <SheetClose asChild key={navlink.url}>
                   <Link
+                    className={`link hover:text-primary pb-1 ${
+                      pathname === navlink.url ? "active" : ""
+                    }`}
+                    key={navlink.url}
                     href={navlink.url}
-                    className="relative hover:text-primary transition-all"
                   >
-                    {navlink.url === pathname && (
-                      <motion.span
-                        initial={{ y: "-100%" }}
-                        animate={{ y: 0 }}
-                        transition={{ type: "tween" }}
-                        layoutId="underline"
-                        className="absolute left-0 top-full h-[2px] bg-primary w-full"
-                      />
-                    )}
                     {navlink.name}
                   </Link>
                 </SheetClose>
@@ -66,7 +144,7 @@ export default function MobileNav() {
                   href={item.url}
                   key={index}
                   target="_blank"
-                  className="text-4xl hover:text-primary"
+                  className="text-4xl hover:text-primary text-foreground transition-all"
                 >
                   {item.name}
                 </a>
