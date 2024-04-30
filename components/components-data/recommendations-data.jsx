b9479819e1eb6ce03fca9a7f6467ebf9c8052b9e
Lucian Rosu <lucian.rosu.atc@gmail.com>
2024-04-30 14:45:01 +0100

    Add new recommendation for Amber Robeck in recommendations-data.jsx

--- components/components-data/recommendations-data.jsx
+++ components/components-data/recommendations-data.jsx
@@ -22,17 +22,17 @@ export const recommendationsData = [
       "Lucian collaborated with us on a project. During this period, he proved that he is an ambitious, hardworking person with good technical skills who has a strong desire to learn even more and improve himself. He works very well in the team and is always willing to help. Lucian is a person I highly recommend.",
   },
   {
-    img: "/img/recommendations/Amber.jpg",
-    name: "Amber Robeck",
-    job:"Full Stack Web Developer",
-    description:
-      "Lucian is a fast learner with a unique approach to solving problems. Lucian is not afraid to dive into challenging tasks with a positive attitude. He is a great contributor to any team. I look forward to working with Lucian in the future and highly recommend him for any projects.",
-  },
-  {
     img: "/img/recommendations/Miguel.jpg",
     name: "Miguel P.Gomez",
     job:"Frontend Developer",
     description:
       "I met Lucian Rosu while we were studying at Scrimba. Lucian is smart, professional, and deadline oriented. Not only did he become a great developer, but he was also encouraging and helping the rest of us to achieve it. He is a team player and highly recommended.",
+  },
+  {
+    img: "/img/recommendations/Amber.jpg",
+    name: "Amber Robeck",
+    job:"Full Stack Web Developer",
+    description:
+      "Lucian is a fast learner with a unique approach to solving problems. Lucian is not afraid to dive into challenging tasks with a positive attitude. He is a great contributor to any team. I look forward to working with Lucian in the future and highly recommend him for any projects.",
   },
 ];
