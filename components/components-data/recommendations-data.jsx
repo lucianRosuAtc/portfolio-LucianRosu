fb946b5f50b3d776c5ebb037b0057b6d807e2385
Lucian Rosu <lucian.rosu.atc@gmail.com>
2024-05-01 13:54:18 +0100

    Update recommendations for Amber Robeck and Kieran Colley in recommendations-data.jsx

--- components/components-data/recommendations-data.jsx
+++ components/components-data/recommendations-data.jsx
@@ -1,38 +1,38 @@
-
 export const recommendationsData = [
   {
-    img: "/img/recommendations/Kieran.jpeg",
-    name: "Kieran Colley",
-    job:"Senior Full Stack React/NextJs Developer @ Adopstar Ltd",
+    img: "/img/recommendations/Amber.jpg",
+    name: "Amber Robeck",
+    job: "Full Stack Web Developer",
     description:
-      "Lucian is a great person to work with, always wanting to learn new things and get stuck in. He has a great personality and a great work ethic.",
+      "Lucian is a fast learner with a unique approach to solving problems. Lucian is not afraid to dive into challenging tasks with a positive attitude. He is a great contributor to any team. I look forward to working with Lucian in the future and highly recommend him for any projects.",
   },
   {
-    img: "/img/recommendations/AlexErnila.jpg",
-    name: "Alex Ernila",
-    job:"Software Developer",
+    img: "/img/recommendations/Jade.jpeg",
+    name: "Jade Page",
+    job: "Senior Web Developer @ Adopstar Ltd",
     description:
-      "Lucian Rosu is one of the best among all the people I have ever worked with. He is a very productive person, hardworking, broad-minded and forward-thinking individual. Intelligent, ambitious, energetic and proactive perfectionist. The desire for proficiency make Lucian a valuable asset to the team.",
+      "An absolute pleasure to work with, Lucian Rosu radiates positivity and kindness, fostering a harmonious and supportive work environment effortlessly.",
   },
   {
     img: "/img/recommendations/Gilson.jpg",
     name: "Gilson de Almeida",
-    job:"Full Stack Developer",
+    job: "Full Stack Developer",
     description:
-      "Lucian collaborated with us on a project. During this period, he proved that he is an ambitious, hardworking person with good technical skills who has a strong desire to learn even more and improve himself. He works very well in the team and is always willing to help. Lucian is a person I highly recommend.",
+    "Lucian collaborated with us on a project. During this period, he proved that he is an ambitious, hardworking person with good technical skills who has a strong desire to learn even more and improve himself. He works very well in the team and is always willing to help. Lucian is a person I highly recommend.",
   },
   {
-    img: "/img/recommendations/Miguel.jpg",
-    name: "Miguel P.Gomez",
-    job:"Frontend Developer",
+    img: "/img/recommendations/AlexErnila.jpg",
+    name: "Alex Ernila",
+    job: "Software Developer",
     description:
-      "I met Lucian Rosu while we were studying at Scrimba. Lucian is smart, professional, and deadline oriented. Not only did he become a great developer, but he was also encouraging and helping the rest of us to achieve it. He is a team player and highly recommended.",
+    "Lucian Rosu is one of the best among all the people I have ever worked with. He is a very productive person, hardworking, broad-minded and forward-thinking individual. Intelligent, ambitious, energetic and proactive perfectionist. The desire for proficiency make Lucian a valuable asset to the team.",
   },
+
   {
-    img: "/img/recommendations/Amber.jpg",
-    name: "Amber Robeck",
-    job:"Full Stack Web Developer",
+    img: "/img/recommendations/Kieran.jpeg",
+    name: "Kieran Colley",
+    job: "Senior Full Stack React/NextJs Developer @ Adopstar Ltd",
     description:
-      "Lucian is a fast learner with a unique approach to solving problems. Lucian is not afraid to dive into challenging tasks with a positive attitude. He is a great contributor to any team. I look forward to working with Lucian in the future and highly recommend him for any projects.",
+      "Lucian is a great person to work with, always wanting to learn new things and get stuck in. He has a great personality and a great work ethic.",
   },
 ];
