1a323166ae50f537d854ece71ce66fe864a1840f
Lucian Rosu <lucian.rosu.atc@gmail.com>
2024-06-03 11:41:31 +0100

    chore: Update npm dependency to latest stable version

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,33 +1,34 @@
+ import { Inter } from "next/font/google";
+ import "./globals.css";
+ import Header from "@/components/Header";
+ import Footer from "@/components/Footer";
+ import { ThemeProvider } from "@/components/ThemeProvider";
+ import { GoogleTagManager } from "@next/third-parties/google";
 
-import Head from 'next/head';
-import { Inter } from "next/font/google";
-import "./globals.css";
-import Header from "@/components/Header";
-import Footer from "@/components/Footer";
-import { ThemeProvider } from "@/components/ThemeProvider";
-import {GoogleTagManager} from "@next/third-parties/google";
+ const inter = Inter({ subsets: ["latin"] });
 
-const inter = Inter({ subsets: ["latin"] });
+ export const metadata = {
+   title: "portfolio Lucian Rosu",
+   description: "This is the portfolio of Lucian Rosu",
+ }
 
-export const metadata = {
-  title: "portfolio Lucian Rosu",
-  description: "This is the portfolio of Lucian Rosu",
-}
 
-export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body className={`${inter.className} max-w-[1920px] mx-auto`}>
+ export default function RootLayout({ children }) {
+   return (
+     <html lang="en">
+       <body className={`${inter.className} max-w-[1920px] mx-auto`}>
         <GoogleTagManager gtmId={`${process.env.GOOGLE_TAG_MANAGER_ID}`} />
-        <ThemeProvider attribute='class'  defaultTheme="system">
-          <Header />
-          {children}
-          <Footer />
-        </ThemeProvider>
-      </body>
-      </html>
-  );
-}
+         <ThemeProvider attribute='class'  defaultTheme="system">
+           <Header />
+           {children}
+           <Footer />
+         </ThemeProvider>
+       </body>
+     </html>
+   );
+ }
+
+
 
 
 
