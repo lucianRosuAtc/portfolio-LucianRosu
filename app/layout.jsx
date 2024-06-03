a7b069704670b08d5353f946682e51d7dc827a71
Lucian Rosu <lucian.rosu.atc@gmail.com>
2024-06-03 11:36:27 +0100

    feat: Add Google Tag Manager script and lang attribute to layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -5,6 +5,7 @@ import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import { ThemeProvider } from "@/components/ThemeProvider";
+import {GoogleTagManager} from "@next/third-parties/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,20 +16,9 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <>
-      <Head>
-      <script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_TAG_MANAGER_ID}`}></script>
-      
-      {/* <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments)}
-        gtag('js', new Date());
-
-        gtag('config', `${process.env.GOOGLE_TAG_MANAGER_ID}`);
-      </script> */}
-      </Head>
-      <html lang="en">
+    <html lang="en">
       <body className={`${inter.className} max-w-[1920px] mx-auto`}>
+        <GoogleTagManager gtmId={`${process.env.GOOGLE_TAG_MANAGER_ID}`} />
         <ThemeProvider attribute='class'  defaultTheme="system">
           <Header />
           {children}
@@ -36,6 +26,10 @@ export default function RootLayout({ children }) {
         </ThemeProvider>
       </body>
       </html>
-    </>
   );
 }
+
+
+
+
+
